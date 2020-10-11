dd1380e8a6762f04665542b9ad99376745b3c6a2
Oscar <lescanooscarpablo@gmail.com>
2020-10-11 15:59:46 -0300

    first commit

---
+++ scenes/models/Platform.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _fall():
+	mode = RigidBody2D.MODE_CHARACTER
+
+func _on_Platform_body_entered(body):
+	$PlatformTimer.start() # Replace with function body.
+
+func _on_PlatformTimer_timeout():
+	_fall() 
